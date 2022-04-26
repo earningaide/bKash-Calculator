e2a6106dae51f77ec172207ab175d3afb039e5b2
bamdyasin <ba.mdyasin@gmail.com>
2022-04-26 05:10:20 -0600

    ned ads

--- app/src/main/java/com/earningaide/p2pcalculator/MainActivity.java
+++ app/src/main/java/com/earningaide/p2pcalculator/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,39 +29,57 @@ public class MainActivity extends AppCompatActivity {
         edtPercent = findViewById(R.id.edtPercent);
         btnCalculate = findViewById(R.id.btnCalculate);
 
+        edtPercent.setText("1.85");
 
+        //On Click Event----------------------
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 //Storing Date
                 inputAmount = edtAmount.getText().toString();
                 inputPercent = edtPercent.getText().toString();
 
-                //Condition
-                if (inputAmount.equals("") || inputPercent.equals("")){
-                    Toast.makeText(MainActivity.this,"ADs Loading...",Toast.LENGTH_SHORT).show();
-                }else {
+                //Condition------------------------------------------
+                    if ( inputAmount.equals("")  ){
+                        edtAmount.setError("Enter Amount");
+                        tvResult.setText("Input Data Missing...\n>> Enter Amount");
 
-                    //Converting Date
-                    fAmount = Float.parseFloat(inputAmount);
-                    fPercent = Float.parseFloat(inputPercent);
+                        }else if ( inputPercent.equals("")  ){
+                            edtPercent.setError("Enter Percent");
+                            tvResult.setText("Input Data Missing...\n>> Enter Percent");
 
-                    // Calculation
-                    fCost = fAmount*fPercent/100;
-                    fTotal = fAmount+fCost;
+                            } else {
 
-                    // Showing Data
-                    tvResult.setText("Your Ordered Amount = Tk."+fAmount+"\nTransection Fee "+fPercent+"% = Tk."+fCost+"\n>> Total Amount = Tk."+fTotal);
-                }
-                //Copy Text
-                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clipData = ClipData.newPlainText("mText",tvResult.getText().toString());
-                clipboardManager.setPrimaryClip(clipData);
-                Toast.makeText(MainActivity.this,"Text Copied",Toast.LENGTH_SHORT).show();
+                                    //Convert String to Float------------------------------
+                                    fAmount = Float.parseFloat(inputAmount);
+                                    fPercent = Float.parseFloat(inputPercent);
+
+                                    // Calculation---------------------------------------
+                                    fCost = fAmount*fPercent/100;
+                                    fTotal = fAmount+fCost;
+
+                                    // Showing Data-------------------------------------
+                                    tvResult.setText("Your Ordered Amount = Tk."+fAmount+"\nTransection Fee "+fPercent+"% = Tk."+fCost+"\n>> Total Amount = Tk."+fTotal+"\nType Last 4 Digit After Payment.");
+                                    edtAmount.setText(""+fAmount);
+
+                                    // SharedPreferences
+                                    SharedPreferences sharedPreferences = getSharedPreferences("spStore", Context.MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("mPercent",inputPercent);
+                                    editor.commit();
+                                    if (sharedPreferences.contains("mPercent")){
+                                        String spPercent = sharedPreferences.getString("mPercent","1.85");
+                                        edtPercent.setText(spPercent);
+                                    }
+                                    //Copy Text---------------------------
+                                    ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                                    ClipData clipData = ClipData.newPlainText("mText",tvResult.getText().toString());
+                                    clipboardManager.setPrimaryClip(clipData);
+                                    Toast.makeText(MainActivity.this,"Text Copied",Toast.LENGTH_LONG).show();
+                        }
             }
         });
-
-
 
     }
 }
